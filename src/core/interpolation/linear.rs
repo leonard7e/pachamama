89443526624ac4676b7716b7b004f2f8a3fe15c4
Leonard Siebeneicher <leonard7e@gmail.com>
2020-07-15 02:51:44 +0200

    Refining definition of VectorSpace

--- src/core/interpolation/linear.rs
+++ src/core/interpolation/linear.rs
@@ -1,6 +1,6 @@
 use crate::core::linear_algebra::vectorspace::VectorSpace;
 
-
 pub fn blend<T:VectorSpace>(a :T, b:T, x: f32) -> T {
+    //Since we do have VectorSpace trait, we only need to define this function once.
     a.scale(x).add(b.scale(1.0-x))
 }
--- src/core/linear_algebra/vectorspace.rs
+++ src/core/linear_algebra/vectorspace.rs
@@ -1,11 +1,33 @@
+/*
+    A Vector Space is defined by two operators.
+    Vector Addition
+    and scalar multiplication.
+
+    We also need a neutral element for addition (zero vector).
+    A neutral element of multiplication is already defined with 1.0
+    or all vector fields. So, we do not need to define it here.
+*/
 
 pub trait VectorSpace {
     fn add(&self, v: Self) -> Self;
-    fn inverse(self) -> Self;
     fn scale(&self, s: f32) -> Self;
+    fn zero() -> Self; // Neutral element for addition
+}
+
+// We could define an invert vector
+pub fn invert<V>(v: V) -> V
+where
+    V: VectorSpace,
+{
+    v.scale(-1.0)
 }
 
 /*
+IDea:
+Introduce
+derive 'Vector'
+to speed up creating vector types.
+
 use std::ops::Add;
 impl <V: VectorSpace> Add<V> for V where V: VectorSpace {
     type Output = Self;
