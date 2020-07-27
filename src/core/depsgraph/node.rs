35c64c76aa7735ebe401ffc9e9d32d7350419ede
Leonard Siebeneicher <leonard7e@gmail.com>
2020-07-27 09:46:33 +0200

    A Node should not contain an evaluator. Instead it should have Evaluator trait itself.

--- src/core/depsgraph/node.rs
+++ src/core/depsgraph/node.rs
@@ -1,11 +1,17 @@
 /*
 
 */
+use evaluator::Evaluator;
 
 use std::arc::Arc;
 use std::rc::RefCell;
 
 struct Node <E: Evaluator>{
-        name: String,
-        evaluator: Arc<RefCell<E>>
+    name: String,
 };
+
+impl Evaluator for Node {
+    fn eval() {
+        unimplemented!()
+    }
+}
