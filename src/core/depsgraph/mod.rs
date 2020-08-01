9b29ed37a5da9944286c0efe4d55c43a9ffb73b3
Leonard Siebeneicher <leonard7e@gmail.com>
2020-08-01 07:42:32 +0200

    Make "Node" a trait.

--- src/core/depsgraph/mod.rs
+++ src/core/depsgraph/mod.rs
@@ -4,4 +4,6 @@
 
 pub mod slot;
 pub mod node;
+
 pub mod graph;
+pub mod procedural_node;
--- src/core/depsgraph/node.rs
+++ src/core/depsgraph/node.rs
@@ -1,31 +1,10 @@
 /*
 
 */
-use crate::core::context::evaluator::Evaluator;
-use crate::core::context::{ptype::PType, Context};
 
-use super::{graph::Graph, slot::Slot};
+use crate::core::utils::typedefs::Key;
 
-pub enum Node {
-    Subgraph(Box<Graph>),
-    Node(NodeRec)
-}
-
-pub struct NodeRec{
-    name: String,
-    procedure: Box<dyn Fn(Context, Vec<Slot>) -> Vec<Slot>>,
-    inputs: Vec<PType>,
-    outputs: Vec<PType>
-}
-
-// Typendescriptors sollten keine Arbeitsdaten enthalten
-
-impl Evaluator for Node {
-    fn eval(&self, context: Context, inputs: Vec<Slot>) -> Vec<Slot>  {
-        match self {
-            Node::Subgraph(g) => g.eval(context, inputs),
-            Node::Node(n) => (n.procedure)(context, inputs),
-        }
-
-    }
+pub trait Node {
+    fn has_input_slot(&self, s: Key) -> bool;
+    fn has_output_slot(&self, s: Key) -> bool;
 }
