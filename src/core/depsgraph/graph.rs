0f3661355020392e29d373aa9093c71f1c18241f
Leonard Siebeneicher <leonard7e@gmail.com>
2020-08-01 08:48:24 +0200

    Adapt Graph node to work with Node trait. Implement Graph node connect.

--- src/core/depsgraph/graph.rs
+++ src/core/depsgraph/graph.rs
@@ -25,15 +25,15 @@ struct Connection {
     to_input: Key,
 }
 
-pub struct Graph {
+pub struct Graph <N: Node> {
     key_counter: Key,
-    nodes: BTreeMap<Key,Arc<Node>>,
+    nodes: BTreeMap<Key,Arc<N>>,
     connections: BTreeSet<Connection>,
     input: Vec<Slot>,
     output: Vec<Slot>,
 }
 
-impl Graph {
+impl <N: Node> Graph<N> {
     fn key_step(&mut self) -> Key {
         let k = self.key_counter;
         self.key_counter += 1;
@@ -44,17 +44,34 @@ impl Graph {
         unimplemented!()
     }
 
-    pub fn insert(&mut self, node: Node) -> Option<Key> {
+    pub fn insert(&mut self, node: N) -> Option<Key> {
         let k = self.key_step();
         self.nodes.insert(k, Arc::new(node));
         Some(k)
     }
 
-    fn connect(&mut self, c: Connection) -> () {
-        self.connections.insert(c);
+    fn connect(&mut self, c: Connection) -> Result<(), &str> {
+        // check if target nodes and target slots exist
+        let from_slot_valid = self.nodes.get(&c.from)
+            .map_or(false, |n|
+                n.as_ref()
+                .has_output_slot(&c.from_output) );
+
+        let to_slot_valid = self.nodes.get(&c.to)
+            .map_or(false, |n|
+                n.as_ref()
+                .has_output_slot(&c.to_input) );
+
+        if from_slot_valid && to_slot_valid {
+            self.connections.insert(c);
+            Ok(())
+        }
+        else {
+            return Err("Graph error: could not establish connection")
+        }
     }
 }
 
-impl Evaluator for Graph {
+impl <N:Node> Evaluator for Graph<N> {
     fn eval(&self, context: Context, inputs: Vec<Slot>) -> Vec<Slot>{ unimplemented!() }
 }
