a001090f724303adbe4db9c46c24702238868859
Leonard Siebeneicher <leonard7e@gmail.com>
2020-08-02 10:00:09 +0200

    Slots are storet in SlotMap's now. Olala. I try to unify iteration over slots without too much hacking into Rust's type system.

--- src/core/depsgraph/node.rs
+++ src/core/depsgraph/node.rs
@@ -4,10 +4,12 @@
 
 use crate::core::{context::{ptype::PType, Context}, utils::typedefs::Key};
 
-use super::slot::Slot;
+use super::slot::{SlotIter, Slot};
 
 pub trait Node <T: PType>{
     fn has_input_slot(&self, s: &Key) -> bool;
     fn has_output_slot(&self, s: &Key) -> bool;
+    fn iter_output_slots(&self) -> Box<SlotIter<T>>;
+    fn iter_input_slots(&self) -> Box<SlotIter<T>>;
     fn eval(&self, context: Context<T>, input: Vec<Slot<T>>) -> Vec<Slot<T>>;
 }
--- src/core/depsgraph/slot.rs
+++ src/core/depsgraph/slot.rs
@@ -2,9 +2,16 @@
 
 */
 
-use crate::core::context::ptype::PType;
+use crate::core::{utils::typedefs::Key, context::ptype::PType};
+use std::collections::BTreeMap;
+use std::collections::btree_map;
 
 pub struct Slot <T: PType>{
     name: String,
     ty: T
 }
+
+
+
+pub type SlotMap <T: PType> = BTreeMap<Key, Slot<T>>;
+pub type SlotIter <'a, T: PType > = btree_map::Iter<'a, Key,Slot<T>>;
