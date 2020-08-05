16f432b30fda60a7149580c242fe0023abd92c85
Leonard Siebeneicher <leonard7e@gmail.com>
2020-08-05 07:50:00 +0200

    Fix struct name of procedural node.

--- src/core/depsgraph/node_procedural.rs
+++ src/core/depsgraph/node_procedural.rs
@@ -2,14 +2,13 @@ use crate::core::{utils::typedefs::Key, context::{ptype::PType, Context}};
 
 use super::{trait_node::Node, slot::{SlotMap, Slot}};
 
-pub struct ProceduralNode<T: PType>{
-    pub name: String,
+pub struct NodeProcedural<T: PType>{
     pub procedure: Box<dyn Fn(Context<T>, Vec<Slot<T>>) -> Vec<Slot<T>>>,
     pub inputs: SlotMap<T>,
     pub outputs: SlotMap<T>
 }
 
-impl <T: PType> Node for ProceduralNode<T> {
+impl <T: PType> Node for NodeProcedural<T> {
     type SlotType = T;
     fn eval(&self, context: Context<T>, inputs: Vec<Slot<T>>) -> Vec<Slot<T>>  {
         (self.procedure)(context, inputs)
